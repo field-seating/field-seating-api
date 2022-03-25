704965ba577e2e0ec8ffcd833fed27815ac69c42
ronnychiang <ronnychiang1164@gmail.com>
2022-03-26 01:02:51 +0800

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const logger = require('./middleware/winston')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json()) // POST json格式
+app.get('/', (req, res) => res.send('Hello field-seating!'))
+app.listen(port, () =>
+  logger.info(`example app listening on port ${port}!`)
+)
