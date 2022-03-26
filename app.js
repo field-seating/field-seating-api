f4c912774cb69151cf865eddf2d20d6370dbc10d
ronnychiang <ronnychiang1164@gmail.com>
2022-03-26 21:00:32 +0800

    feat: postEcho test api

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const logger = require('./middleware/winston')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json()) // POST json格式
+
+
+require('./routes')(app)
 app.get('/', (req, res) => res.send('Hello field-seating!'))
 app.listen(port, () =>
   logger.info(`example app listening on port ${port}!`)
