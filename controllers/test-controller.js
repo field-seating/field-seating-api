a71cc716fc1ae9a9d894f27293fd08ec0a70f809
Wendell Liu <cuk.bas@gmail.com>
2022-03-28 14:25:53 +0800

    chore: test

--- controllers/test-controller.js
+++ controllers/test-controller.js
@@ -2,6 +2,7 @@ const testController = {
   postEcho: (req, res) => {
     console.log(req.body);
     const a = 1;
+    const b = 1;
     return res.status(200).json({ status: 'success', echo: req?.body?.msg });
   },
 };
