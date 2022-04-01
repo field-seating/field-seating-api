88f8f19347f8c9e8b6e7879426e407d35b7e7227
Wendell Liu <cuk.bas@gmail.com>
2022-04-01 15:09:34 +0800

    fix: handle nullish input and throw error outside the unknown error

--- services/user-service.js
+++ services/user-service.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs');
+const R = require('ramda');
+
 const generalError = require('../controllers/helpers/general-error');
 const signUpErrorMap = require('../errors/signUpError');
 const UserModel = require('../models/user');
@@ -6,12 +8,16 @@ const UserModel = require('../models/user');
 const userServices = {
   signUp: async (name, email, password) => {
     // check input
-    if (!email.trim()) throw new generalError(signUpErrorMap['emailRequired']);
-    if (!name.trim()) throw new generalError(signUpErrorMap['nameRequired']);
-    if (!password.trim())
+    if (R.isNil(email) || !email.trim())
+      throw new generalError(signUpErrorMap['emailRequired']);
+    if (R.isNil(name) || !name.trim())
+      throw new generalError(signUpErrorMap['nameRequired']);
+    if (R.isNil(password) || !password.trim())
       throw new generalError(signUpErrorMap['passwordRequired']);
+
     // hash password
     const hash = await bcrypt.hash(password, 10);
+
     // create user
     const userModel = new UserModel();
     const data = {
@@ -23,8 +29,10 @@ const userServices = {
       const postUser = await userModel.createUser(data);
       return postUser;
     } catch (err) {
-      if (err.code === 'P2002')
+      if (err.code === 'P2002') {
         throw new generalError(signUpErrorMap['duplicateEmail']);
+      }
+      throw err;
     }
   },
 };
