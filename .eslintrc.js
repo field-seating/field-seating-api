a66d4d828d9ba2815133922a492218999126fefe
Wendell Liu <cuk.bas@gmail.com>
2022-04-03 15:47:11 +0800

    feat: dummpy test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,20 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: ['eslint:recommended', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'jest'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
     'prettier/prettier': ['error'],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
   ignorePatterns: ['node_modules/**/*.js'],
 };
