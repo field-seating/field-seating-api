93f74be15643d552325438e3f705be1242b214b4
Wendell Liu <cuk.bas@gmail.com>
2022-04-13 23:28:49 +0800

    feat: UTC timestamp

--- src/config/logger/helper.js
+++ src/config/logger/helper.js
@@ -4,11 +4,11 @@ const { mergeRight } = require('ramda');
 const config = require('../config');
 
 const { log: logConfig } = config;
-const timestampFormatter = format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' });
 
 const jsonStructureFormatter = format.printf(
-  ({ timestamp, level, message, url, requestId, ...rest }) => {
+  ({ level, message, url, requestId, ...rest }) => {
     const requestIdOutput = requestId ? { requestId } : {};
+    const timestamp = new Date().toISOString();
 
     const output = mergeRight(
       {
@@ -33,7 +33,7 @@ const fileTransport = new transports.DailyRotateFile({
   zippedArchive: true,
   maxSize: '20m',
   maxFiles: `${logConfig.maxFilesDays}d`,
-  format: format.combine(timestampFormatter, jsonStructureFormatter),
+  format: format.combine(jsonStructureFormatter),
 });
 
 const fileErrorTransport = new transports.DailyRotateFile({
@@ -44,14 +44,13 @@ const fileErrorTransport = new transports.DailyRotateFile({
   zippedArchive: true,
   maxSize: '20m',
   maxFiles: `${logConfig.maxFilesDays}d`,
-  format: format.combine(timestampFormatter, jsonStructureFormatter),
+  format: format.combine(jsonStructureFormatter),
 });
 
 const consoleTransport = new transports.Console({
   level: logConfig.maxLevel,
   handleExceptions: true,
   format: format.combine(
-    timestampFormatter,
     jsonStructureFormatter,
     format.colorize({ all: true })
   ),
@@ -61,6 +60,4 @@ module.exports = {
   fileTransport,
   fileErrorTransport,
   consoleTransport,
-  timestampFormatter,
-  jsonStructureFormatter,
 };
