726201869d86045d9a0351d9ee290c9465e5c637
Wendell Liu <cuk.bas@gmail.com>
2022-04-13 19:06:52 +0800

    feat: request logger

--- src/middleware/request-logger.js
+++ src/middleware/request-logger.js
@@ -3,9 +3,12 @@ const logger = require('../config/logger');
 
 const requestLogger = morgan((token, req) => {
   const body = req.body;
+  const url = req.url;
+
   logger.info('request in', {
     requestId: req.requestId,
     body,
+    url,
   });
 });
 
