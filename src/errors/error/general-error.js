c0f5f3202416bffbd5f572dd9a6b92f85f6658a3
Wendell Liu <cuk.bas@gmail.com>
2022-04-14 00:18:17 +0800

    feat: remove capture stack trace

--- src/errors/error/general-error.js
+++ src/errors/error/general-error.js
@@ -5,7 +5,6 @@ class GeneralError extends Error {
     this.name = this.constructor.name;
     this.code = code;
     this.httpCode = httpCode;
-    //Error.captureStackTrace(this, this.constructor.name);
   }
 }
 
