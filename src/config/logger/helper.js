2b40f6bf13b6eb78d505c41c416dd3f27b49165a
Wendell Liu <cuk.bas@gmail.com>
2022-04-20 20:16:10 +0800

    Refine the log (#15)
    * feat: no color
    
    * chore: trigger image build
    
    * feat: remove timestamp; add http method
    
    * chore: rollback

--- src/config/logger/helper.js
+++ src/config/logger/helper.js
@@ -1,63 +1,37 @@
 const { format, transports } = require('winston');
-const { mergeRight } = require('ramda');
 
 const config = require('../config');
 
 const { log: logConfig } = config;
 
 const jsonStructureFormatter = format.printf(
-  ({ level, message, url, requestId, ...rest }) => {
-    const requestIdOutput = requestId ? { requestId } : {};
-    const timestamp = new Date().toISOString();
-
-    const output = mergeRight(
-      {
-        level,
-        url,
-        message,
-        ...rest,
-        timestamp,
-      },
-      requestIdOutput
-    );
+  ({ level, message, httpUrl, httpMethod, requestId, ...rest }) => {
+    const output = {
+      level,
+      httpUrl,
+      httpMethod,
+      message,
+      ...rest,
+      requestId,
+    };
 
     return `${JSON.stringify(output)}`;
   }
 );
 
-const fileTransport = new transports.DailyRotateFile({
-  level: logConfig.maxLevel,
-  dirname: 'logs',
-  filename: `application-%DATE%.log`,
-  datePattern: 'YYYY-MM-DD-HH',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: `${logConfig.maxFilesDays}d`,
-  format: format.combine(jsonStructureFormatter),
-});
+let consoleFormatList = [jsonStructureFormatter];
 
-const fileErrorTransport = new transports.DailyRotateFile({
-  level: 'error',
-  dirname: 'logs',
-  filename: `error-%DATE%.log`,
-  datePattern: 'YYYY-MM-DD-HH',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: `${logConfig.maxFilesDays}d`,
-  format: format.combine(jsonStructureFormatter),
-});
+if (process.env.NODE_ENV !== 'production') {
+  consoleFormatList = consoleFormatList.concat(format.colorize({ all: true }));
+}
 
 const consoleTransport = new transports.Console({
   level: logConfig.maxLevel,
   handleExceptions: logConfig.handleExceptions,
-  format: format.combine(
-    jsonStructureFormatter,
-    format.colorize({ all: true })
-  ),
+  format: format.combine(...consoleFormatList),
+  stderrLevels: ['error'],
 });
 
 module.exports = {
-  fileTransport,
-  fileErrorTransport,
   consoleTransport,
 };
