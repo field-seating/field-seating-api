46b5273b7abcaacd0d548e9ca581bd798aebad2d
ronnychiang <ronnychiang1164@gmail.com>
2022-04-29 19:54:08 +0800

    fix: fix migration

---
+++ prisma/migrations/20220411121415_add_status_for_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Users` ADD COLUMN `status` VARCHAR(255) NOT NULL DEFAULT 'notVerify';
---
+++ prisma/migrations/20220415162005_fix_user_status_value/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Users` MODIFY `status` VARCHAR(255) NOT NULL DEFAULT 'unverified';
