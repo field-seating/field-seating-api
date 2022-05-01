d32f90c27324bab2b433d352ae6bda97ea8ade8e
Wendell Liu <cuk.bas@gmail.com>
2022-05-01 17:20:33 +0800

    feat: password service testing

--- src/services/password-service/password-service.test.js
+++ src/services/password-service/password-service.test.js
@@ -2,10 +2,20 @@ const assert = require('assert/strict');
 
 const PasswordService = require('./');
 const passwordErrorMap = require('../../errors/password-error');
+const UserModel = require('../../models/user');
+const PasswordRestTokenModel = require('../../models/password-reset-token');
+const prisma = require('../../config/prisma');
 
 const passwordService = new PasswordService({ logger: console });
+const userModel = new UserModel();
+const passwordResetTokenModel = new PasswordRestTokenModel();
 
-describe('password-service', () => {
+afterEach(async () => {
+  await passwordResetTokenModel._truncate();
+  await userModel._truncate();
+});
+
+describe('recoveryPassword', () => {
   it('should throw error when no email matched', () => {
     const email = 'email';
 
@@ -17,5 +27,24 @@ describe('password-service', () => {
         code: passwordErrorMap.emailInvalid.code,
       }
     );
+  });
+
+  it('should create a passwordResetToken entity', async () => {
+    const user = await userModel.createUser({
+      email: 'email',
+      name: 'name',
+      password: 'pwd',
+    });
+
+    await passwordService.recoveryPassword(user.email);
+
+    const entities = await prisma.passwordResetTokens.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    expect(entities).toHaveLength(1);
+    expect(entities[0].userId).toBe(user.id);
   });
 });
