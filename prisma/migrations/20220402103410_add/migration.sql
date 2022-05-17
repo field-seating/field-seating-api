902678d31313829e24565ff92d724f9caa7b6af9
ronnychiang <ronnychiang1164@gmail.com>
2022-05-17 18:02:01 +0800

    fix: migration

--- prisma/migrations/20220402103410_add/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `Fields` MODIFY `img` VARCHAR(255) NULL;
-
--- AlterTable
-ALTER TABLE `Records` MODIFY `score` VARCHAR(10) NULL,
-    MODIFY `comment` VARCHAR(255) NULL;
--- prisma/migrations/20220517100109_post_photo/migration.sql
+++ prisma/migrations/20220517100109_post_photo/migration.sql
@@ -1,7 +1,6 @@
 /*
   Warnings:
 
-  - You are about to drop the column `spaceId` on the `Photos` table. All the data in the column will be lost.
   - You are about to drop the column `thumbnail_path` on the `Photos` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[path]` on the table `Photos` will be added. If there are existing duplicate values, this will fail.
   - Made the column `userId` on table `PasswordResetTokens` required. This step will fail if there are existing NULL values in that column.
@@ -13,22 +12,19 @@ ALTER TABLE `PasswordResetTokens` DROP FOREIGN KEY `PasswordResetTokens_userId_f
 -- DropForeignKey
 ALTER TABLE `Photos` DROP FOREIGN KEY `Photos_recordId_fkey`;
 
--- DropForeignKey
-ALTER TABLE `Photos` DROP FOREIGN KEY `Photos_spaceId_fkey`;
-
--- DropIndex
-DROP INDEX `Photos_spaceId_path_key` ON `Photos`;
-
--- DropIndex
-DROP INDEX `Photos_spaceId_thumbnail_path_key` ON `Photos`;
+-- AlterTable
+ALTER TABLE `Fields` MODIFY `img` VARCHAR(255) NULL;
 
 -- AlterTable
 ALTER TABLE `PasswordResetTokens` MODIFY `userId` INTEGER NOT NULL;
 
 -- AlterTable
-ALTER TABLE `Photos` DROP COLUMN `spaceId`,
-    DROP COLUMN `thumbnail_path`,
+ALTER TABLE `Photos` DROP COLUMN `thumbnail_path`,
     MODIFY `recordId` INTEGER NULL;
+
+-- AlterTable
+ALTER TABLE `Records` MODIFY `score` VARCHAR(10) NULL,
+    MODIFY `comment` VARCHAR(255) NULL;
 
 -- CreateIndex
 CREATE UNIQUE INDEX `Photos_path_key` ON `Photos`(`path`);
