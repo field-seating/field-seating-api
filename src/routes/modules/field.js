dd0536ba3a67bae9883c0e7496b73a00d18d7bfb
ronnychiang <ronnychiang1164@gmail.com>
2022-05-29 17:40:03 +0800

    feat: route

--- src/routes/modules/field.js
+++ src/routes/modules/field.js
@@ -3,6 +3,8 @@ const fieldController = require('../../controllers/field-controller');
 
 const router = express.Router();
 
+router.get('/:id/levels', fieldController.getFields);
+router.get('/:id/orientations', fieldController.getFields);
 router.get('/', fieldController.getFields);
 
 module.exports = router;
