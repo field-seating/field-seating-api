1b0f30935d94fee14675e629b51025fec630b5fa
Wendell Liu <cuk.bas@gmail.com>
2022-06-23 01:28:31 +0800

    feat: update asset domain (#45)

--- src/services/photo-service.js
+++ src/services/photo-service.js
@@ -48,7 +48,7 @@ class PhotoService extends BaseService {
           );
           const data = {
             ...photo,
-            url: `https://${assetDomain}.com${photo.path}`,
+            url: `https://${assetDomain}${photo.path}`,
           };
           const result = R.omit(['path'], data);
           return result;
