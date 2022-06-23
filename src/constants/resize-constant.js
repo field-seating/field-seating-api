b8870396f422696f3491a76eaefea2e0c3f58d67
Wendell Liu <cuk.bas@gmail.com>
2022-06-23 23:53:04 +0800

    Feat: Image consume path renderer (#46)
    * refactor: image uploading flow
    
    * fix: update test
    
    * chore: clean the code
    
    * chore: rollback the module
    
    * feat: make resizeImage a public function
    
    * test: responsive tool
    
    * refactor: function design
    
    * chore: clear

--- src/constants/resize-constant.js
+++ src/constants/resize-constant.js
@@ -1,11 +1,38 @@
 const sizeMap = {
-  seatPhoto: [
-    { size: 640, namePrefix: 'thumb_' },
-    { size: 3200, namePrefix: '' },
-  ],
+  seatPhoto: {
+    xs: {
+      resizeOption: {
+        width: 160,
+        withoutEnlargement: true,
+      },
+      format: 'webp',
+      formatOption: { quality: 50 },
+    },
+    sm: {
+      resizeOption: {
+        width: 320,
+        withoutEnlargement: true,
+      },
+      format: 'webp',
+      formatOption: { quality: 70 },
+    },
+    md: {
+      resizeOption: {
+        width: 640,
+        withoutEnlargement: true,
+      },
+      format: 'webp',
+      formatOption: { quality: 70 },
+    },
+    lg: {
+      resizeOption: {
+        width: 1280,
+        withoutEnlargement: true,
+      },
+      format: 'jpeg',
+      formatOption: { quality: 90 },
+    },
+  },
 };
 
-const formatMap = {
-  jpeg90: { format: 'jpeg', quality: 90 },
-};
-module.exports = { sizeMap, formatMap };
+module.exports = { sizeMap };
---
+++ src/utils/upload-image/responsive.test.js
@@ -0,0 +1,40 @@
+const { renderResizeInfo, renderDataset } = require('./responsive');
+
+const sizeMap = {
+  flag1: {
+    value: 1,
+  },
+  flag2: {
+    value: 2,
+  },
+};
+
+describe('renderResizeInfo', () => {
+  const resizeInfo = renderResizeInfo(sizeMap)({ filename: 'foo.jpeg' });
+
+  it('should have proper list size', () => {
+    expect(resizeInfo).toHaveLength(2);
+  });
+
+  it('should contain the original option', () => {
+    expect(resizeInfo).toContainEqual({
+      config: { value: 2 },
+      filename: 'flag2/foo.jpeg',
+    });
+  });
+});
+
+describe('renderDataset', () => {
+  const dataset = renderDataset(sizeMap)({
+    path: 'foo.jpeg',
+    bucketName: 'staging/photos',
+    assetDomain: 'https://example.cdn.com',
+  });
+
+  it('should contain the data', () => {
+    expect(dataset).toEqual({
+      flag1: 'https://example.cdn.com/staging/photos/flag1/foo.jpeg',
+      flag2: 'https://example.cdn.com/staging/photos/flag2/foo.jpeg',
+    });
+  });
+});
