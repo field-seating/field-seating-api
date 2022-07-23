103bc0592806d9937d15fb5156d4a8d37a86cd78
ronnychiang <ronnychiang1164@gmail.com>
2022-07-23 17:05:51 +0800

    fix: merge sql migration

--- prisma/migrations/20220415162005_fix_user_status_value/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Users` MODIFY `status` VARCHAR(255) NOT NULL DEFAULT 'unverified';
--- prisma/migrations/20220723090439_version_one/migration.sql
+++ prisma/migrations/20220723090439_version_one/migration.sql
@@ -2,13 +2,18 @@
 CREATE TABLE `Users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(80) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `role` VARCHAR(255) NOT NULL DEFAULT 'user',
+    `status` VARCHAR(255) NOT NULL DEFAULT 'unverified',
+    `verificationToken` VARCHAR(255) NULL,
+    `tokenCreatedAt` DATETIME(3) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `Users_email_key`(`email`),
+    UNIQUE INDEX `Users_name_key`(`name`),
+    UNIQUE INDEX `Users_verificationToken_key`(`verificationToken`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -16,10 +21,11 @@ CREATE TABLE `Users` (
 CREATE TABLE `Fields` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
-    `img` VARCHAR(255) NOT NULL,
+    `img` VARCHAR(255) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `Fields_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -30,6 +36,7 @@ CREATE TABLE `Orientations` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `Orientations_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -51,6 +58,7 @@ CREATE TABLE `Levels` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `Levels_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -75,28 +83,33 @@ CREATE TABLE `Zones` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `Zones_fieldId_name_key`(`fieldId`, `name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Spaces` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) NULL,
     `zoneId` INTEGER NOT NULL,
     `spaceType` VARCHAR(255) NOT NULL,
     `version` VARCHAR(255) NOT NULL,
-    `colNumber` VARCHAR(10) NOT NULL,
-    `rowNumber` VARCHAR(10) NOT NULL,
+    `colNumber` INTEGER NULL,
+    `rowNumber` INTEGER NULL,
+    `positionColNumber` INTEGER NOT NULL,
+    `positionRowNumber` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `Spaces_id_spaceType_key`(`id`, `spaceType`),
+    UNIQUE INDEX `Spaces_zoneId_version_positionColNumber_positionRowNumber_key`(`zoneId`, `version`, `positionColNumber`, `positionRowNumber`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Seats` (
     `spaceId` INTEGER NOT NULL,
-    `spaceType` VARCHAR(191) NOT NULL DEFAULT 'seat',
+    `spaceType` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -106,7 +119,27 @@ CREATE TABLE `Seats` (
 -- CreateTable
 CREATE TABLE `Pillars` (
     `spaceId` INTEGER NOT NULL,
-    `spaceType` VARCHAR(191) NOT NULL DEFAULT 'pillar',
+    `spaceType` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`spaceId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Groups` (
+    `spaceId` INTEGER NOT NULL,
+    `spaceType` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`spaceId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Aisles` (
+    `spaceId` INTEGER NOT NULL,
+    `spaceType` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -119,8 +152,8 @@ CREATE TABLE `Records` (
     `userId` INTEGER NOT NULL,
     `spaceId` INTEGER NOT NULL,
     `date` DATE NOT NULL,
-    `score` VARCHAR(10) NOT NULL,
-    `comment` VARCHAR(255) NOT NULL,
+    `score` VARCHAR(10) NULL,
+    `comment` VARCHAR(255) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -132,13 +165,13 @@ CREATE TABLE `Photos` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
     `spaceId` INTEGER NOT NULL,
-    `recordId` INTEGER NOT NULL,
-    `date` DATE NOT NULL,
+    `recordId` INTEGER NULL,
     `path` VARCHAR(255) NOT NULL,
-    `thumbnail_path` VARCHAR(255) NOT NULL,
+    `date` DATE NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `Photos_path_key`(`path`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -158,11 +191,27 @@ CREATE TABLE `Reviews` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `userId` INTEGER NOT NULL,
     `photoId` INTEGER NOT NULL,
-    `useful` VARCHAR(255) NOT NULL,
+    `useful` VARCHAR(255) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
     INDEX `Reviews_photoId_useful_idx`(`photoId`, `useful`),
+    UNIQUE INDEX `Reviews_userId_photoId_key`(`userId`, `photoId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `PasswordResetTokens` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `token` VARCHAR(255) NULL,
+    `state` ENUM('INVALID', 'VALID') NOT NULL DEFAULT 'INVALID',
+    `tokenSignedAt` DATETIME(3) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `PasswordResetTokens_token_key`(`token`),
+    INDEX `PasswordResetTokens_state_idx`(`state`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -197,6 +246,12 @@ ALTER TABLE `Seats` ADD CONSTRAINT `Seats_spaceId_spaceType_fkey` FOREIGN KEY (`
 ALTER TABLE `Pillars` ADD CONSTRAINT `Pillars_spaceId_spaceType_fkey` FOREIGN KEY (`spaceId`, `spaceType`) REFERENCES `Spaces`(`id`, `spaceType`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE `Groups` ADD CONSTRAINT `Groups_spaceId_spaceType_fkey` FOREIGN KEY (`spaceId`, `spaceType`) REFERENCES `Spaces`(`id`, `spaceType`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Aisles` ADD CONSTRAINT `Aisles_spaceId_spaceType_fkey` FOREIGN KEY (`spaceId`, `spaceType`) REFERENCES `Spaces`(`id`, `spaceType`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `Records` ADD CONSTRAINT `Records_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -209,7 +264,7 @@ ALTER TABLE `Photos` ADD CONSTRAINT `Photos_userId_fkey` FOREIGN KEY (`userId`) 
 ALTER TABLE `Photos` ADD CONSTRAINT `Photos_spaceId_fkey` FOREIGN KEY (`spaceId`) REFERENCES `Spaces`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Photos` ADD CONSTRAINT `Photos_recordId_fkey` FOREIGN KEY (`recordId`) REFERENCES `Records`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Photos` ADD CONSTRAINT `Photos_recordId_fkey` FOREIGN KEY (`recordId`) REFERENCES `Records`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Collections` ADD CONSTRAINT `Collections_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -222,3 +277,6 @@ ALTER TABLE `Reviews` ADD CONSTRAINT `Reviews_userId_fkey` FOREIGN KEY (`userId`
 
 -- AddForeignKey
 ALTER TABLE `Reviews` ADD CONSTRAINT `Reviews_photoId_fkey` FOREIGN KEY (`photoId`) REFERENCES `Photos`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PasswordResetTokens` ADD CONSTRAINT `PasswordResetTokens_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
