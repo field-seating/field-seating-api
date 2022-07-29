b31bd741c80a49d985ce9b9725370304638fff08
Wendell Liu <wendell.liu@houzz.com>
2022-07-29 17:26:20 +0800

    feat: handle case

--- src/config/config.js
+++ src/config/config.js
@@ -15,7 +15,9 @@ const configFromEnv = {
   redisUrl: process.env.REDIS_URL,
   sendgridApiKey: process.env.SENDGRID_API_KEY,
   sendgridSandboxMode:
-    process.env.SENDGRID_SANDBOX_MODE === 'true' ? true : false,
+    (process.env.SENDGRID_SANDBOX_MODE || '').toLowerCase() === 'true'
+      ? true
+      : false,
 };
 
 const envMap = {
