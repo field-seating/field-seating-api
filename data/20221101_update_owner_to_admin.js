125d2a5215fafac57e796a3fe87d0d2f61728f5b
ronnychiang <ronnychiang1164@gmail.com>
2022-11-02 11:18:06 +0800

    fix: better code

--- data/20221101_update_owner_to_admin.js
+++ data/20221101_update_owner_to_admin.js
@@ -1,7 +1,5 @@
 const updateToAdmin = require('./utils/update-to-admin');
 
-const owner1Id = 1;
-const owner2Id = 2;
+const ownerId = [1, 2];
 
-updateToAdmin(owner1Id);
-updateToAdmin(owner2Id);
+updateToAdmin(ownerId);
--- data/utils/update-to-admin.js
+++ data/utils/update-to-admin.js
@@ -2,19 +2,23 @@ const UserModel = require('../../src/models/user');
 const logger = require('../../src/config/logger');
 const { isNil } = require('ramda');
 
-async function updateToAdmin(userId) {
-  logger.info(`update user:${userId} to admin`);
-
+async function updateToAdmin(ownerId) {
   const userModel = new UserModel();
-  // check userId existed
-  const user = await userModel.getUserById(userId);
-  if (!isNil(user)) {
+  ownerId.forEach(async (owner) => {
+    logger.info(`update user:${owner} to admin`);
+
+    // check userId existed
+    const user = await userModel.getUserById(owner);
+
+    // if userId is not existed
+    if (isNil(user)) {
+      return logger.info(`user:${owner} not found`);
+    }
+
     // update to admin
     await userModel.updateToAdmin(user.id);
     logger.info(`success update user:${user.email} to admin`);
-  }
-  // if userId is not existed
-  logger.info(`user:${userId} not found`);
+  });
 }
 
 module.exports = updateToAdmin;
